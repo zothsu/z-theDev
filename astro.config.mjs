5b46127d9b2f84a60e0fb6f5e6d880567555d68e
Zoe Sullivan <zoetsullivan@gmail.com>
2025-05-15 12:34:41 -0700

    Adds static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
   compressHTML: true,
   site: 'https://z-thedev.xyz',
   integrations: [
